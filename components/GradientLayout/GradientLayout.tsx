a56b83602adfc533f4fd70dfbce76e814c641314
ruhollahh <ruhollahh01@gmail.com>
2022-04-10 02:01:56 +0430

    add gradient layout

---
+++ components/GradientLayout/GradientLayout.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Box, Flex, Heading, Text, VStack } from '@chakra-ui/layout';
+import { Image } from '@chakra-ui/react';
+
+const GradientLayout: React.FC<{
+	image: string;
+	color: string;
+	title: string;
+	subtitle: string;
+	description: string;
+	isImageRound: boolean;
+}> = ({
+	image,
+	color,
+	title,
+	subtitle,
+	description,
+	isImageRound = false,
+	children,
+}) => {
+	return (
+		<VStack
+			h="full"
+			alignItems="stretch"
+			bgGradient={`linear(to-b, ${color}.500 0%, ${color}.600 15%, ${color}.700 45%, rgba(0, 0, 0, 0.95) 75%)`}
+		>
+			<Flex gap={5} p={5} paddingTop={12}>
+				<Image
+					boxSize="3xs"
+					src={image}
+					borderRadius={isImageRound ? 'full' : 'none'}
+				/>
+				<VStack alignSelf="flex-end" alignItems="flex-start" color="white">
+					<Text textTransform="uppercase" fontSize="sm">
+						{subtitle}
+					</Text>
+					<Heading fontSize="5xl">{title}</Heading>
+					<Text fontSize="sm">{description}</Text>
+				</VStack>
+			</Flex>
+			<Box flexGrow={1} bgColor="blackAlpha.600">
+				{children}
+			</Box>
+		</VStack>
+	);
+};
+
+export { GradientLayout };
--- components/PlayerLayout/PlayerLayout.tsx
+++ components/PlayerLayout/PlayerLayout.tsx
@@ -4,15 +4,36 @@ import { Sidebar } from '../Sidebar';
 const PlayerLayout = ({ children }) => {
 	return (
 		<VStack alignItems="stretch" h="100%" spacing={0}>
-			<HStack alignItems="stretch" h="calc(100% - 100px)">
+			<HStack alignItems="stretch" h="calc(100% - 100px)" spacing="0">
 				<Box flexBasis="250px">
 					<Sidebar />
 				</Box>
-				<Box flexGrow={1}>{children}</Box>
+				<Box
+					flexGrow={1}
+					h="100%"
+					overflowY="auto"
+					sx={{
+						'&::-webkit-scrollbar': {
+							width: '10px',
+						},
+						'&::-webkit-scrollbar-track': {
+							backgroundColor: 'transparent',
+						},
+						'&::-webkit-scrollbar-thumb': {
+							backgroundColor: 'gray.800',
+							borderRadius: '20px',
+							border: '2px solid transparent',
+							backgroundClip: 'content-box',
+						},
+						'&::-webkit-scrollbar-thumb:hover': {
+							backgroundColor: 'gray.700',
+						},
+					}}
+				>
+					{children}
+				</Box>
 			</HStack>
-			<Box minH="100px" bgColor="green.300">
-				Player
-			</Box>
+			<Box minH="100px">Player</Box>
 		</VStack>
 	);
 };
